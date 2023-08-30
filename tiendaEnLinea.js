e1026b54bf78b43a0ec8e944560c207646b64665
teshy18 <sebas.tesitore@gmail.com>
2023-08-30 20:39:47 -0300

    Tienda en Linea

--- tiendaEnLinea.js
+++ tiendaEnLinea.js
@@ -1,3 +1,14 @@
 console.log('')
 console.log('Ejercicio 3: Tienda en línea')
 
+let producto = {
+    nombre: 'Moto G42', 
+    precio: '$150.000',
+    disponible: true
+}
+
+const mostrarProducto = function(producto){
+    console.log(JSON.stringify(producto, null, 2));
+}
+
+mostrarProducto(producto)
